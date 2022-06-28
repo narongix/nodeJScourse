fde08af91368cd53e784e912502f1ab49400466c
nrayix <narongix@gmail.com>
2022-06-28 15:45:01 +0700

    updates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,43 +3,56 @@ const path = require('path')
 const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../templates/views')
-const partialsPath = path.join(__dirname,'../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.use(express.static(publicDirectoryPath))
 
 // Setup handlebars engine and views location
-app.set('view engine','hbs')
-app.set('views',viewsPath)
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
-app.get('',(req,res)=>{
-    res.render('index',{
-        title:'Weather App',
-        name:'Andrew Mead'
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Andrew Mead'
     })
 })
 
-app.get('/about',(req,res)=>{
-    res.render('about',{
-        title:'About me',
-        name:'Andrew Mead'
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Andrew Mead'
     })
 })
 
-app.get('/help',(req,res)=>{
-    res.render('help',{
-        title:'Help',
-        helpText:'This is some helpful text',
-        name:'Andrew Mead'
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpText: 'This is some helpful text',
+        name: 'Andrew Mead'
     })
 })
 
 
-app.get('/weather',(req,res)=>{
-    res.send('Weather')
+app.get('/help/*', (req, res) => {
+    res.render('404',{
+        title:'404',
+        name: 'Andrew Mead',
+        errorMessage:'Help article not found'
+    })
 })
-app.listen(3000,()=>{
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
